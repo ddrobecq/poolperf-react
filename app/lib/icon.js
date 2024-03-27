96fbd6cb3fdb8c49101139e4f16b0879c48ce031
ddrobecq <didier@drobecq.fr>
2024-03-27 17:10:55 +0100

    Retry to style PoolTriangleIcon

--- app/lib/icon.js
+++ app/lib/icon.js
@@ -1,13 +1,14 @@
-import styled from 'styled-components'
+//import styled from 'styled-components'
 import { GiPoolTriangle } from "react-icons/gi";
 
-const PoolTriangleIcon  = styled(GiPoolTriangle)`
+/*const PoolTriangleIcon  = styled(GiPoolTriangle)`
   transform: scale(2);
   margin: 5%;
 `;
-  
+  */
+
 export default function PoolIcon(props) {
     return (
-        <PoolTriangleIcon />
+        <GiPoolTriangle  fontSize={25} />
     );
 }
