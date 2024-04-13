c84cdb5ab800afbe591de3a444d721bec4d7443d
ddrobecq <didier@drobecq.fr>
2024-04-13 19:30:29 +0200

    Fix background color for header accoding to the theme

--- app/home/header.js
+++ app/home/header.js
@@ -2,10 +2,13 @@
 
 import Image from 'next/image';
 import { AppBar, Stack } from '@mui/material';
+import { useTheme } from '@emotion/react';
 
 export default function Header (props) {
+    const theme = useTheme();
+
     return (
-        <AppBar position={'sticky'} sx={{ marginBottom: 1, backgroundImage:'none'}}>
+        <AppBar position={'sticky'} sx={{ marginBottom: 1, backgroundImage:'none', backgroundColor: theme.palette.background.default}}>
             <Stack width={'100%'} spacing={1} direction={'column'} alignItems={'center'} >
                 <Image src={'/header.png'} alt={'title'} width={250} height={50}/>
             </Stack>
